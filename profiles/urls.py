9ed729fdf7d37519008a815901270215257913a3
Ana Popovic <franjetina@gmail.com>
2024-01-06 18:34:31 +0100

    path fixed

--- profiles/urls.py
+++ profiles/urls.py
@@ -7,6 +7,6 @@ app_name = "profiles"
 urlpatterns = [
     path("<str:username>/", views.ProfileDetailView.as_view(), name="detail"),
     path("<str:username>/follow/", views.FollowView.as_view(), name="follow"),
-    path("<str:username>/edit/", views.EditProfileView.as_view(),
+    path("profiles/<str:username>/edit/", views.EditProfileView.as_view(),
     name="edit"),
 ]
